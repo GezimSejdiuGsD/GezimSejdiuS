32dc3eb756a6ba072e8bff9ec1ec4c09f04a31b5
GezimSejdiuGsD <76073533+GezimSejdiuGsD@users.noreply.github.com>
2021-06-15 04:59:45 +0200

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            THIS IS MY FIRST PROJECT 
+        </div>
+        <ul>  
+        <li>
+        Name: Gëzim Sejdiu
+        </li>
+        <li>
+        Date of birth: 13.10.1995
+        </li>
+        <li>
+        email: gezimsejdiu95@gmail.com
+        </li>
+        <li>
+        Nationality: Albanian 
+        </li>
+        </ul>
+        <li></li>
+    <div> Work Experience</div>
+        <ul>
+        Purchase Officer: "NewCo Ferronikeli"
+        <ul>From December 2020 to May 2021</ul>
+        <li> Market Research </li>
+        <li> Compare and evaluate offers from suppliers </li>
+        <li> Negotiate contract terms of agreement and pricing </li>
+        <li> Maintaint updated records of purchased products, delivery information and invoices </li>
+        <li> Prepare reports on purchases including cost analyses </li>
+        <li> Monitor stock levels and place orders as needed </li>
+        </ul>
+
+        <ul>
+        Warehouse and Fulfillment/Customer Support "Gjirafa INC"
+        <ul>From August 2020 to December 2021</ul>
+        <li> Stock maintenance </li>
+        <li> Local transport organization </li>
+        <li> Invoicing process </li>
+        <li> Customer support </li>
+        </ul>
+        
+        <ul>
+        Penetration Tester intern "Seccon"
+        <ul>From April 2020 to July 2021</ul>
+        <li> Testing of computer systems </li>
+        <li> Testing of network systems </li>
+        <li> Testing of Web & Mobile applications  </li>
+        </ul>
+        
+        <ul>
+        Spare Parts Manager "Man TRUCK & BUS Kosovo"
+        <ul>From July 2019 to February 2020</ul>
+        <li> Manage of spare parts department </li>
+        <li> Stock maintenance </li>
+        <li> Sales of spare parts </li>
+        <li> Invoicing process </li>
+        <li> Organization of international and local transport </li>
+        </ul>
+        
+        <ul>
+        Technical Support "Euronet"
+        <ul>From May 2017 to November 2018</ul>
+        <li> Maintenance of technological equipment's on daily bases </li>
+        <li> Sales of technological equipment </li>
+        <li> Customer service </li>
+        </ul>
+    <div> Skills </div>
+        <ul>
+        <li> Problem Solving </li>
+        <li> Administration </li>
+        <li> Communication </li>
+        </ul>
+    <div> Computer Skills </div>
+        <ul>
+        <li> Cyber Security, Ethical Hacking </li>
+        <li> PHP/Laravel, Mysql</li>
+        </ul>
+    <div> Languages </div>
+        <ul>
+        <li> Albanian </li>
+        <li> English </li>
+        <li> German </li>
+        </ul>
+    <div> Certifications & Courses </div>
+        <ul> 
+        <li> Cyber security, "ICT for Kosovo's Growth" 2021</li>
+        <li> PHP/Laravel & Mysql online lessons </li>
+        <li> Cyber Security course "Roy Academy" 2020 </li>
+        <li> Video Game Design, "Cactus Education" 2018</li>
+        </ul>
+    <div> Interests </div>
+        <ul>    
+        <li> Travelling </li>
+        <li> Gaming </li>
+        <li> Sports </li>
+        </ul>
+    
+    </div>
+@endsection
